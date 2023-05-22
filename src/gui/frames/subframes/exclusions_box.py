7bc04d73c66e10b4809b8a3271c2f0eddda59c7d
jonathanreno <jonathanreno30@gmail.com>
2023-05-21 20:58:54 -0400

    changed anycronous switches func to concurrent func with threading mod

--- src/gui/frames/subframes/exclusions_box.py
+++ src/gui/frames/subframes/exclusions_box.py
@@ -1,8 +1,5 @@
-import tracemalloc
-tracemalloc.start()
-
 import customtkinter
-import asyncio
+import threading
 from constants.constants import (
     CORNER_RADIUS_0, WIDTH_250, NSEW, PADY_1, PADY_1, GRID_ROW_1, GRID_COL_2
 )
@@ -12,6 +9,8 @@ from constants.net_script_scan import net_script_scan_list
 class ExclusionsBox(customtkinter.CTkTabview):
     def __init__(self, master):
         super().__init__(master)
+
+        self.scrollable_frame_switches = []
 
         # style config
         self._corner_radius = CORNER_RADIUS_0
@@ -40,7 +39,8 @@ class ExclusionsBox(customtkinter.CTkTabview):
             self.tab(PRANGE_1), text="CTkLabel on Tab 2")
         self.label_tab_1.grid(row=0, column=0, padx=20, pady=20)
         self.string_input_button = customtkinter.CTkButton(self.tab(PRANGE_1), text="Load Advanced Options",
-                                                           command=self.run_load_switches)
+        
+                                                           command=None)
         self.string_input_button.grid(row=1, column=0, padx=20, pady=(20, 10))
 
         self.label_tab_2 = customtkinter.CTkLabel(
@@ -51,29 +51,25 @@ class ExclusionsBox(customtkinter.CTkTabview):
             self.tab(PRANGE_3), text="Select Vulnerability Scan")
         self.label_tab_3.grid(row=0, column=0, padx=20, pady=20)
 
-    # Create script switches 
-    async def create_switches_async(self, name_list, switch_list):
-        switches = []
-        for i, element in enumerate(name_list):
-            switch = customtkinter.CTkSwitch(self.scrollable_frame, text=element)
-            switch.grid(row=i, column=0, padx=10, pady=(0, 20), sticky="w")
-            switch.bind("<Button-1>", self.testfunc)
-            switch_list.append(switch)
-            switches.append(switch)
-        return switches 
-    
-    async def load_switches(self):
-            # Call create_switches_async() as an await-able function
-            switch_list = []
-            switches = await self.create_switches_async(net_script_scan_list, switch_list)
-            print("Switches created:", switches)
+
+        self.create_switches_thread = threading.Thread(target=self.create_switches, args=(net_script_scan_list, self.scrollable_frame_switches))
+        self.create_switches_thread.start()
+
+        #self.scrollable_frame_switches = []
+    # This function is threaded due to it's long blocking time
+    def create_switches(self, names_list, switch_list):        
         
-    def run_load_switches(self):
-        asyncio.run(self.load_switches())
-
-
+        for i, element in enumerate(names_list):
+            self.switch = customtkinter.CTkSwitch(self.scrollable_frame, text=element)
+            self.switch.grid(row=i, column=0, padx=10, pady=(0, 20), sticky="w")
+            self.switch.bind("<Button-1>", self.testfunc)
+            switch_list.append(self.switch)
             
-    
+            
+
+        
+        #self.create_switches_thread.join()
+
     def testfunc(self):
         print("switch_clicked")
    
