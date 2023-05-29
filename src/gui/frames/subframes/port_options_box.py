c9bf9a083a1b05f3078cf2fea9d64c204cadacc8
jonathanreno <jonathanreno30@gmail.com>
2023-05-29 12:37:17 -0400

    mods made; switch replaces radiobtn3

--- src/gui/frames/subframes/port_options_box.py
+++ src/gui/frames/subframes/port_options_box.py
@@ -16,7 +16,7 @@ class PortOptionsBox(customtkinter.CTkFrame):
 
         self.grid(row=1, column=3, padx=(PADX_1, 20), pady=(PADY_1, 0), sticky=NSEW)
 
-        # styling config
+        # styling configure
         self._corner_radius = CORNER_RADIUS_0
 
         # setup port variables key/value pairs
@@ -36,75 +36,26 @@ class PortOptionsBox(customtkinter.CTkFrame):
         self.toplevel_window = None # err msg handler
         self.entry_start = None
         self.entry_end = None
-        self.submit_button = None
+        #self.submit_button = None
         self.previous_selection = ""
         self.created_widgets  = []
         
         # set default selected value for radio buttons
         self.radio_var = tkinter.StringVar(value=self.first_value)
+        self.switch_var = customtkinter.StringVar(value="off")
 
         self.label_radio_group = customtkinter.CTkLabel(self,  text="Port Range")
         self.label_radio_group.grid(row=0, column=2, columnspan=1, padx=PADX_2, pady=PADY_2, sticky="")
         self.radio_button_1 = customtkinter.CTkRadioButton(self, text=self.first_key, variable=self.radio_var, value=self.first_value, command=self.radio_button_command)
         self.radio_button_1.grid(row=1, column=2, pady=10, padx=20, sticky="n",columnspan=1)
         self.radio_button_2 = customtkinter.CTkRadioButton(self, text=self.second_key, variable=self.radio_var, value=self.second_value, command=self.radio_button_command)
-        self.radio_button_2.grid(row=GRID_ROW_2, column=GRID_COL_2, pady=PADY_2, padx=PADX_1, sticky=N,columnspan=1)
-        self.radio_button_3 = customtkinter.CTkRadioButton(self, text=self.third_key, variable=self.radio_var, value=self.third_value, command=self.enable_entry_fields)
-        self.radio_button_3.grid(row=3, column=2, pady=10, padx=20, sticky="n", columnspan=1)
-
-        self.start_value = tkinter.StringVar()
-        self.end_value = tkinter.StringVar()
-
-        self.entry_start = customtkinter.CTkEntry(self, placeholder_text="From...", state = tkinter.DISABLED, textvariable=self.start_value)
-        self.entry_start.grid(row=4, column=2, columnspan=1, padx=(10, 10), pady=(10, 10), sticky="nsew")
-        self.entry_end = customtkinter.CTkEntry(self, placeholder_text="To...", state = tkinter.DISABLED, textvariable=self.end_value)
-        self.entry_end.grid(row=5, column=2, columnspan=1, padx=(10, 10), pady=(10, 10), sticky="nsew")
-        self.submit_button = customtkinter.CTkButton(self, text="submit", state = tkinter.DISABLED, command=None) 
-        self.submit_button.grid(row=6, column=2, columnspan=1, padx=(10, 10), pady=(10, 10), sticky="nsew")
-
-
-    def radio_button_command(self):
-        self.current_selection = self.radio_var.get()  # Get the selected radio button value
-        if self.current_selection != self.previous_selection:
-
-            if self.current_selection == self.first_value:
-                self.args_list = [self.first_value]
-                print(self.first_value, " type ", type(self.first_value))
-                     
-                
-            elif self.current_selection == self.second_value:
-                self.args_list = [self.second_value]
-                print(self.second_value, " type ", type(self.second_value))
-
-            elif self.current_selection == self.third_value:
-                print(self.third_value, " type ", type(self.third_value))
-            
-
-            self.previous_selection = self.current_selection
-        else: 
-            if self.current_selection != "Option 3":
-                self.remove_entry_fields()
-
-        print(self.args_list)
-
-
-
-    def remove_entry_fields(self):
+        self.radio_button_2.grid(row=GRID_ROW_2, column=GRID_COL_2, pady=PADY_2, padx=PADX_1, sticky=N, columnspan=1)
         
-        if  self.entry_start is not None:
-            self.entry_start.destroy()
-            self.entry_start = None
+        self.switch = customtkinter.CTkSwitch(self, text=self.third_key, variable=self.switch_var, onvalue="on", offvalue="off", command=self.switch_event)
+        self.switch.grid(row=3, column=2, pady=PADY_2, padx=PADX_1, sticky="n", columnspan=1)
 
-        if  self.entry_end is not None:
-            self.entry_end.destroy()
-            self.entry_end = None
+        
 
-        if self.submit_button is not None:
-           self.submit_button.destroy()
-           self.submit_button= None
-
-    
-    def enable_entry_fields(self):
         self.start_value = tkinter.StringVar()
         self.end_value = tkinter.StringVar()
         
@@ -112,7 +63,40 @@ class PortOptionsBox(customtkinter.CTkFrame):
         self.entry_start.grid(row=4, column=2, columnspan=1, padx=(10, 10), pady=(10, 10), sticky="nsew")
         self.entry_end = customtkinter.CTkEntry(self, placeholder_text="To...", textvariable=self.end_value)
         self.entry_end.grid(row=5, column=2, columnspan=1, padx=(10, 10), pady=(10, 10), sticky="nsew")
-        self.submit_button = customtkinter.CTkButton(self, text="submit", command=None) 
+        self.submit_button = customtkinter.CTkButton(self, text="submit", command=self.radio_button_command) 
         self.submit_button.grid(row=6, column=2, columnspan=1, padx=(10, 10), pady=(10, 10), sticky="nsew")
 
+        self.entry_start.configure(state=tkinter.DISABLED)
+        self.entry_end.configure(state=tkinter.DISABLED)
+        self.submit_button.configure(state=tkinter.DISABLED)
 
+        print(self.args_list)
+
+        
+    def radio_button_command(self):
+        pass
+
+    def switch_event(self):
+        self.switch_state = self.switch_var.get()
+        
+        if self.switch_state == "on":
+            # Disable radio buttons
+            self.radio_button_1.configure(state=tkinter.DISABLED)
+            self.radio_button_2.configure(state=tkinter.DISABLED)
+
+            # Enable entry fields and submit button
+            self.entry_start.configure(state=tkinter.NORMAL)
+            self.entry_end.configure(state=tkinter.NORMAL)
+            self.submit_button.configure(state=tkinter.NORMAL)
+        else:
+            # Enable radio buttons
+            self.radio_button_1.configure(state=tkinter.NORMAL)
+            self.radio_button_2.configure(state=tkinter.NORMAL)
+
+            # Disable entry fields and submit button
+            self.entry_start.configure(state=tkinter.DISABLED)
+            self.entry_end.configure(state=tkinter.DISABLED)
+            self.submit_button.configure(state=tkinter.DISABLED)
+
+
+        
