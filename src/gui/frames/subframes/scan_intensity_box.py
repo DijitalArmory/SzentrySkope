fb7799f78e36c10bc70a32ebf07486834cbaebca
jonathanreno <jonathanreno30@gmail.com>
2023-06-19 17:55:44 -0400

    seg-buttons removed; sliders realigned

--- src/gui/frames/subframes/scan_intensity_box.py
+++ src/gui/frames/subframes/scan_intensity_box.py
@@ -18,33 +18,58 @@ class ScanIntensityBox(customtkinter.CTkFrame):
         self._corner_radius = CORNER_RADIUS_0
         self.fg_color = TRANSPARENT
         
-        self.previous_selection = ""
+        self.previous_value = ""
         self.seg_var = tkinter.StringVar(value=SCAN_1_INT)
 
         # create segmented button 1
-        self.seg_button_1 = customtkinter.CTkSegmentedButton(self, command=self.show_present_silder)
+        '''elf.seg_button_1 = customtkinter.CTkSegmentedButton(self, variable=self.seg_var, command=self.show_present_silder)
         self.seg_button_1.grid(row=GRID_ROW_0, column=GRID_COL_0, padx=(PADX_1, 10), pady=(PADY_2, 10), sticky=EW)
         self.seg_button_1.configure(values=[SCAN_1_INT, SCAN_2_INT, SCAN_3_INT])
-        self.seg_button_1.set(value=1)
+        self.seg_button_1.set(value=1)'''
         
-
+        
         self.slider_1 = customtkinter.CTkSlider(self, from_=0, to=5, number_of_steps=5)
         self.slider_1.grid(row=1, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
         self.slider_1.set(0)
-
+        self.slider_1.bind("<B1-Motion>", lambda event: self.on_slider_move(self.slider_1.get()))
+        self.stealth_label = customtkinter.CTkLabel(self, text="Stealth")
+        self.stealth_label.grid(row=1, column=0, padx=(10, 10), pady=(1, 1), sticky="ns")
+        
         self.slider_2 = customtkinter.CTkSlider(self, from_=0, to=9,number_of_steps=9)
         self.slider_2.grid(row=2, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
-        
-        self.slider_3 = customtkinter.CTkSlider(self, from_=1, to=9,number_of_steps=9)
-        self.slider_3.grid(row=3, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
+        self.slider_2.bind("<B1-Motion>", lambda event: self.on_slider_move(self.slider_2.get()))
+        self.aggression_label = customtkinter.CTkLabel(self, text="Aggression")
+        self.aggression_label.grid(row=2, column=0, padx=(10, 10), pady=(1, 1), sticky="ns")
 
+        self.slider_3 = customtkinter.CTkSlider(self, from_=0, to=9,number_of_steps=9)
+        self.slider_3.grid(row=3, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
+        self.slider_3.bind("<B1-Motion>", lambda event: self.on_slider_move(self.slider_3.get()))
+        self.evasion_label = customtkinter.CTkLabel(self, text="Evasion")
+        self.evasion_label.grid(row=3, column=0, padx=(10, 10), pady=(1, 1), sticky="ns")
+
+    def on_slider_move(self, value):
+        seg_var = self.seg_var.get()
+        if value != self.previous_value:            #print("Slider value:", value)
+            print("Slider value:", value)
+            self.previous_value = value
 
     def show_present_silder(self, event=None):
         pass
+        '''self.current_selection = self.seg_var.get()
+        if self.current_selection == SCAN_1_INT:
+            print(SCAN_1_INT)
+            self.slider_1.configure(state=tkinter.NORMAL)
+            self.slider_2.configure(state=tkinter.DISABLED)
+            self.slider_3.configure(state=tkinter.DISABLED)
+        elif self.current_selection == SCAN_2_INT:
+            print(SCAN_2_INT)
+            self.slider_1.configure(state=tkinter.DISABLED)
+            self.slider_2.configure(state=tkinter.NORMAL)
+            self.slider_3.configure(state=tkinter.DISABLED)
+        elif self.current_selection == SCAN_3_INT:
+            print(SCAN_3_INT)
+            self.slider_1.configure(state=tkinter.DISABLED)
+            self.slider_2.configure(state=tkinter.DISABLED)
+            self.slider_3.configure(state=tkinter.NORMAL)'''
 
-    '''
-    LEFT OFF
-    sliders need to be differentiated from one another somehow visually
-    based on which segment is selected
-    06/15/2023
-    '''
+    
